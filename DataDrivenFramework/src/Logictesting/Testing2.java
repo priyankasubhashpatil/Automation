e32eca94987df85d451c52571ee9377dfe9d5755
priyankasubhashpatil <hemangipatil2712@gmail.com>
2019-04-07 17:28:02 +0530

    comitting all the files

---
+++ DataDrivenFramework/src/Logictesting/Testing2.java
@@ -0,0 +1,19 @@
+package Logictesting;
+
+public class Testing2 
+{
+
+	public static void main(String[] args)
+	{
+	System.out.println( System.getProperty("user.dir"));
+
+	String str="E:\\workspace\\DataDrivenFramework"+"\\BrowserDrivers\\chromedriver.exe";
+	String str2=System.getProperty("user.dir")+"\\BrowserDrivers\\chromedriver.exe";
+	
+	System.out.println(str);
+	System.out.println(str2);
+	
+	
+	}
+
+}
